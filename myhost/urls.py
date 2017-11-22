a4ba730bb5bd8e0adabbffc2ddfc32cc057f7305
shide123 <376189717@qq.com>
2017-11-22 17:44:24 +0800

    okay11222

--- myhost/urls.py
+++ myhost/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
 from .views import *
-from .hostinfo import *
 
 urlpatterns = [
-    url(r'^machineinfo/$', GetPhyHostInfoView.as_view, name='machineinfo'),
+    url(r'^machineinfo/$', getHostInfo, name='machineinfo'),
     url(r'^phylist/$', PhyhostListView.as_view(), name='phyhost_list'),
     url(r'^addphy/$', AddPhyHostView.as_view(), name='addphy'),
     url(r'^phydetail/(?P<physicalIp>\d+)/$', PhyhostDetailView.as_view(), name='phyhost_detail'),
@@ -11,6 +10,4 @@ urlpatterns = [
     url(r'^addvir/$', AddVirHostView.as_view(), name='addvir'),
     url(r'virdetail/(?P<virtualIp>\d+)/$', VirhostDetailView.as_view(), name='virhost_detail'),
     url(r'^viralllist/$', VirhostList_allView.as_view(), name='virhost_list'),
-
-
 ]
--- myhost/views.py
+++ myhost/views.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect
 from pure_pagination import PageNotAnInteger, EmptyPage, Paginator
 from django.views.generic.base import View
 from myhost.models import *
@@ -9,6 +8,7 @@ from myhost.forms import *
 from django.shortcuts import render
 import json, commands
 import logging
+
 
 # Create your views here.
 class PhyhostListView(View):
@@ -33,15 +33,16 @@ class PhyhostDetailView(View):
 
 class VirhostList_allView(View):
     def get(self, request):
-        #all_virtual = VirtualMachine.objects.all().order_by("virtualIp")
-        #try:
+        # all_virtual = VirtualMachine.objects.all().order_by("virtualIp")
+        # try:
         #    page = request.GET.get('page', 1)
-        #except PageNotAnInteger:
+        # except PageNotAnInteger:
         #    page = 1
-        #p = Paginator(all_virtual, 3, request=request)
-        #virtual_list = p.page(page)
-        #return render(request, 'myVirHost.html', {'all_virtual', virtual_list})
+        # p = Paginator(all_virtual, 3, request=request)
+        # virtual_list = p.page(page)
+        # return render(request, 'myVirHost.html', {'all_virtual', virtual_list})
         return render(request, 'myVirHost.html')
+
 
 class VirhostListView(View):
     def get(self, request, physicalIp):
@@ -64,8 +65,7 @@ class VirhostDetailView(View):
 
 class AddPhyHostView(View):
     def get(self, request):
-        phy_forms = PhysicalForm()
-        return render(request, 'phyhost_forms.html', {'phy_forms': phy_forms})
+        return render(request, 'phyhost_forms.html')
 
     def post(self, request):
         phy_forms = PhysicalForm(request.POST)
@@ -74,16 +74,113 @@ class AddPhyHostView(View):
             machineRoom_address = request.POST.get('machineRoom_address', '')
             machineRoom_attr = request.POST.get('machineRoom_attr', '')
             machine_info = request.POST.get('machine_info', '')
-            phyhost = PhysicalMachine.objects.filter(physicalIp=physicalIp, machineRoom_address=machineRoom_address, machineRoom_attr=machineRoom_attr)
+            phyhost = PhysicalMachine.objects.filter(physicalIp=physicalIp, machineRoom_address=machineRoom_address,
+                                                     machineRoom_attr=machineRoom_attr)
             if phyhost is not None:
                 phyhost.save()
-                return HttpResponseRedirect('/hosts/phylist/')
+                response = HttpResponsePermanentRedirect(reversed('myhost:phyhost_list'))
+                return response
 
-class GetPhyHostInfoView(View):
-    def post(self, request):
-        return
-    def get(self, request):
-        return
+
+def getHostInfo(request):
+    if request.method == "GET":
+        physicalIp = request.GET.get("physicalIp_input")
+        phyhost = PhysicalMachine.objects.filter(physicalIp=physicalIp)
+        if phyhost is not None:
+            (status, result) = commands.getstatusoutput('sh /tmp/test.sh %s' % physicalIp)
+            if status == 0:
+                all_the_text = open('/tmp/ansible_info.txt').read()
+                data = json.loads(all_the_text)
+                try:
+                    data = data["ansible_facts"]
+                except Exception, e:
+                    logging.error('ansible setup模块获取数据失败:%s' % e)
+                    return "error"
+                    # 物理内存容量
+                try:
+                    mem_total = data["ansible_memtotal_mb"]
+                except Exception, e:
+                    logging.error('ansible setup模块获取物理内存容量数据失败:%s' % e)
+                    mem_total = 0
+                    # 虚拟内容容量
+                try:
+                    swap_total = data["ansible_memory_mb"]["swap"]["total"]
+                except Exception, e:
+                    logging.error('ansible setup模块获取虚拟内容容量数据失败:%s' % e)
+                    swap_total = 0
+                    # CPU型号
+                try:
+                    cpu_type = data["ansible_processor"][-1]
+                except Exception, e:
+                    logging.error('ansible setup模块获取CPU型号数据失败:%s' % e)
+                    cpu_type = "未知"
+                    # CPU核心数
+                try:
+                    cpu_total = data["ansible_processor_vcpus"]
+                except Exception, e:
+                    logging.error('ansible setup模块获取CPU核心数数据失败:%s' % e)
+                    cpu_total = 0
+                    # 操作系统类型
+                try:
+                    os_type = " ".join((data["ansible_distribution"], data["ansible_distribution_version"]))
+                except Exception, e:
+                    logging.error('ansible setup模块获取操作系统类型数据失败:%s' % e)
+                    os_type = "未知"
+                    # 硬盘总容量
+                try:
+                    disk_total = sum([int(data["ansible_devices"][i]["sectors"]) * \
+                                      int(data["ansible_devices"][i]["sectorsize"]) / 1024 / 1024 / 1024 \
+                                      for i in data["ansible_devices"] if i[0:2] in ("sd", "ss")])
+                except Exception, e:
+                    logging.error('ansible setup模块获取硬盘总容量数据失败:%s' % e)
+                    disk_total = 0
+                    # 硬盘挂载名及容量
+                try:
+                    disk_mount = str(
+                        [{"mount": i["mount"], "size": i["size_total"] / 1024 / 1024 / 1024} for i in
+                         data["ansible_mounts"]])
+                except Exception, e:
+                    logging.error('ansible setup模块获取硬盘挂载名及容量数据失败:%s' % e)
+                    disk_mount = "未知"
+                    # 服务器型号
+                try:
+                    server_type = data["ansible_product_name"]
+                except Exception, e:
+                    logging.error('ansible setup模块获取服务器型号数据失败:%s' % e)
+                    server_type = "未知"
+                    # 服务器主机名
+                try:
+                    host_name = data["ansible_hostname"]
+                except Exception, e:
+                    logging.error('ansible setup模块获取服务器主机名数据失败:%s' % e)
+                    host_name = "未知"
+                    # 操作系统内核型号
+                try:
+                    os_kernel = data["ansible_kernel"]
+                except Exception, e:
+                    logging.error('ansible setup模块获取操作系统内核型号数据失败:%s' % e)
+                    os_kernel = "未知"
+                    # 服务器ipv4地址
+                try:
+                    ip4 = []
+                    ip4 = data["ansible_all_ipv4_addresses"]
+
+                except Exception, e:
+                    logging.error('ansible setup模块获取服务器ipv4地址数据失败:%s' % e)
+                    ipv4 = "未知"
+            machine_info = Machine_info.objects.filter(ipv4=ip4[0], cpu_total=cpu_total, mem_total=mem_total,
+                                                       cpu_type=cpu_type, disk_mount=disk_mount,
+                                                       disk_total=disk_total,
+                                                       host_name=host_name, os_kernel=os_kernel,
+                                                       server_type=server_type,
+                                                       swap_total=swap_total)
+            if machine_info is None:
+                machine_info = Machine_info(ipv4=ip4[0], cpu_total=cpu_total, mem_total=mem_total,
+                                            cpu_type=cpu_type,
+                                            disk_mount=disk_mount, disk_total=disk_total, host_name=host_name,
+                                            os_kernel=os_kernel, server_type=server_type, swap_total=swap_total)
+                machine_info.save()
+            return render(request, 'phyhost_forms.html', {'machine_info_input': machine_info})
 
 
 class AddVirHostView(View):
